6042f004b8435b4cd808cb9988a83794b2ca6c4b
Tankerch <mukmin.lc@gmail.com>
2022-08-29 23:26:43 +0700

    Using window screen at config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class AppConfig:
     box_width = 300
-    start_player_y_point = 520
+    start_player_y_point = 500
     players_section_w = 1270
     players_section_h = 350
